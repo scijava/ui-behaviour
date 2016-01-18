4034766afaed1da7babfa53396a9fd7894026144
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2016-01-18 14:16:55 +0100

    InputTriggerDescriptionsBuilder now merges InputTriggerDescriptions with matching actions and contexts.

--- src/main/java/bdv/behaviour/io/InputTriggerDescriptionsBuilder.java
+++ src/main/java/bdv/behaviour/io/InputTriggerDescriptionsBuilder.java
@@ -1,6 +1,8 @@
 package bdv.behaviour.io;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -28,17 +30,34 @@ public class InputTriggerDescriptionsBuilder
 	public List< InputTriggerDescription > getDescriptions()
 	{
 		final ArrayList< InputTriggerDescription > descs = new ArrayList<>();
+		final String[] emptyStringArray = new String[ 0 ];
 
 		for ( final Entry< String, Set< Input > > entry : config.actionToInputsMap.entrySet() )
 		{
 			final Set< Input > inputs = entry.getValue();
 			for ( final Input input : inputs )
 			{
-				final InputTriggerDescription desc = new InputTriggerDescription(
-						new String[] { input.trigger.toString() },
-						input.behaviour,
-						input.contexts.toArray( new String[ 0 ] ) );
-				descs.add( desc );
+				boolean found = false;
+				for ( final InputTriggerDescription desc : descs )
+				{
+					if ( input.behaviour.equals( desc.getAction() ) &&
+							input.contexts.equals( new HashSet<>( Arrays.asList( desc.getContexts() ) ) ) )
+					{
+						final HashSet< String > triggers = new HashSet<>( Arrays.asList( desc.getTriggers() ) );
+						triggers.add( input.trigger.toString() );
+						desc.setTriggers( triggers.toArray( emptyStringArray ) );
+						found = true;
+						break;
+					}
+				}
+				if ( !found )
+				{
+					final InputTriggerDescription desc = new InputTriggerDescription(
+							new String[] { input.trigger.toString() },
+							input.behaviour,
+							input.contexts.toArray( emptyStringArray ) );
+					descs.add( desc );
+				}
 			}
 		}
 
