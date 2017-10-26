df86ef0e8d55e296a18bc7d933cc14590643937e
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2017-10-26 17:04:04 +0200

    Rework the InputTriggerPanelEditor.

--- src/main/java/org/scijava/ui/behaviour/io/gui/InputTriggerPanelEditor.java
+++ src/main/java/org/scijava/ui/behaviour/io/gui/InputTriggerPanelEditor.java
@@ -8,7 +8,6 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -27,33 +26,33 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
 
-public class KeyBindingsPanelEditor extends JPanel
+import org.scijava.ui.behaviour.InputTrigger;
+
+public class InputTriggerPanelEditor extends JPanel
 {
 
 	@FunctionalInterface
-	public static interface KeyBindingsChangeListener
+	public static interface InputTriggerChangeListener
 	{
-		public void keyBindingsChanged();
+		public void inputTriggerChanged();
 	}
 
 	private static final long serialVersionUID = 1L;
 
 	private static final String COMMIT_ACTION = "commit";
 
-	protected final List< String > selectedKeys;
-
-	protected final List< KeyItem > keyItems;
+	private final List< KeyItem > keyItems;
 
 	private final JTextField textField;
 
-	private final boolean editable;
+	private final HashSet< InputTriggerChangeListener > listeners;
 
-	private final HashSet< KeyBindingsChangeListener > listeners;
+	private InputTrigger trigger = InputTrigger.NOT_MAPPED;
 
-	public KeyBindingsPanelEditor( final boolean editable )
+	private String invalidTriggerStr = null;
+
+	public InputTriggerPanelEditor( final boolean editable )
 	{
-		this.editable = editable;
-		this.selectedKeys = new ArrayList<>();
 		this.keyItems = new ArrayList<>();
 		this.listeners = new HashSet<>();
 
@@ -74,7 +73,7 @@ public class KeyBindingsPanelEditor extends JPanel
 			final Autocomplete autoComplete = new Autocomplete();
 			textField.getDocument().addDocumentListener( autoComplete );
 			textField.getInputMap().put( KeyStroke.getKeyStroke( KeyEvent.VK_ENTER, 0 ), COMMIT_ACTION );
-			textField.getInputMap().put( KeyStroke.getKeyStroke( KeyEvent.VK_SPACE, 0 ), COMMIT_ACTION );
+			textField.getInputMap().put( KeyStroke.getKeyStroke( ' ' ), COMMIT_ACTION );
 			textField.getActionMap().put( COMMIT_ACTION, autoComplete.new CommitAction() );
 			textField.addKeyListener( new KeyAdapter()
 			{
@@ -85,9 +84,9 @@ public class KeyBindingsPanelEditor extends JPanel
 					 * We have to use a key listener to deal separately with
 					 * character removal and tag removal.
 					 */
-					if ( e.getKeyCode() == KeyEvent.VK_BACK_SPACE && textField.getText().isEmpty() && !selectedKeys.isEmpty() )
+					if ( e.getKeyCode() == KeyEvent.VK_BACK_SPACE && textField.getText().isEmpty() && !keyItems.isEmpty() )
 					{
-						removeTag( selectedKeys.get( selectedKeys.size() - 1 ) );
+						removeLastKeyItem();
 						e.consume();
 					}
 				}
@@ -98,37 +97,161 @@ public class KeyBindingsPanelEditor extends JPanel
 		add( Box.createHorizontalGlue() );
 	}
 
-	public void setKeys( final Collection< String > tags )
+	public void setInputTrigger( final InputTrigger trigger )
 	{
-		for ( final KeyItem keyItem : keyItems )
-			remove( keyItem );
-		selectedKeys.clear();
-		keyItems.clear();
-
-		for ( final String tag : tags )
-			addKey( tag );
-		revalidate();
-		repaint();
+		this.trigger = trigger;
+		this.invalidTriggerStr = null;
+		regenKeyPanels();
 	}
 
-	protected void addKey( final String key )
+	public InputTrigger getInputTrigger()
 	{
-		final KeyItem tagp = new KeyItem( key, INPUT_TRIGGER_SYNTAX_TAGS.contains( key ) );
-		selectedKeys.add( key );
-		keyItems.add( tagp );
-		add( tagp, getComponentCount() - 2 );
+		return trigger;
 	}
 
-	private void removeTag( final String tag )
+	private void checkAndAppendKey( final String key )
 	{
-		final int index = selectedKeys.indexOf( tag );
-		if ( index < 0 )
+		final String trimmed = key.trim();
+		if ( trimmed.equals( "|" ) )
+		{
+			/*
+			 * Special case: the user wants to enter keys to ignore.
+			 */
+
+			final String str = ( null == trigger || trigger == InputTrigger.NOT_MAPPED )
+					? trimmed
+					: trigger.toString() + " " + trimmed;
+
+			this.trigger = null;
+			this.invalidTriggerStr = str.trim();
+			regenKeyPanels();
+			return;
+		}
+
+		// Find its proper case equivalent in the syntax list.
+		String properCapKey = null;
+		for ( int i = 0; i < INPUT_TRIGGER_SYNTAX_TAGS.size(); i++ )
+		{
+			if ( INPUT_TRIGGER_SYNTAX_TAGS.get( i ).equalsIgnoreCase( trimmed ) )
+			{
+				properCapKey = INPUT_TRIGGER_SYNTAX_TAGS.get( i );
+				break;
+			}
+		}
+
+		if ( null != properCapKey )
+		{
+			// Try to append the key to the trigger.
+			final String str = ( null == trigger )
+					? invalidTriggerStr + " " + properCapKey
+					: ( trigger == InputTrigger.NOT_MAPPED )
+							? properCapKey
+							: trigger.toString() + " " + properCapKey;
+
+			try
+			{
+				this.trigger = InputTrigger.getFromString( str );
+				this.invalidTriggerStr = null;
+			}
+			catch ( final IllegalArgumentException iae )
+			{
+				this.trigger = null;
+				this.invalidTriggerStr = str.trim();
+			}
+			regenKeyPanels();
+		}
+	}
+
+	private void removeLastKeyItem()
+	{
+		// Try to remove the last key item.
+		final String[] tokens;
+		if ( null == trigger )
+		{
+			/*
+			 * The trigger was invalid. In that case we show what would be the
+			 * key sequence in red.
+			 */
+			tokens = invalidTriggerStr.split( " " );
+		}
+		else
+		{
+			/*
+			 * The trigger field is valid.
+			 */
+			tokens = trigger.toString().split( " " );
+		}
+		if ( tokens.length == 0 )
 			return;
 
-		selectedKeys.remove( index );
-		final KeyItem tagPanel = keyItems.remove( index );
+		final StringBuilder strBlder = new StringBuilder();
+		for ( int i = 0; i < tokens.length - 1; i++ )
+			strBlder.append( tokens[ i ] + " " );
+
+		final String str = strBlder.toString();
+		try
+		{
+			this.trigger = str.isEmpty()
+					? InputTrigger.NOT_MAPPED
+					: InputTrigger.getFromString( str );
+			this.invalidTriggerStr = null;
+		}
+		catch ( final IllegalArgumentException iae )
+		{
+			this.trigger = null;
+			this.invalidTriggerStr = str.trim();
+		}
+		regenKeyPanels();
 		notifyListeners();
-		remove( tagPanel );
+	}
+
+	private void regenKeyPanels()
+	{
+		// Clear
+		for ( final KeyItem keyItem : keyItems )
+			remove( keyItem );
+		keyItems.clear();
+
+		final String[] tokens;
+		boolean valid;
+		if ( null == trigger )
+		{
+			/*
+			 * The trigger was invalid. In that case we show what would be the
+			 * key sequence in red.
+			 */
+			tokens = invalidTriggerStr.split( " " );
+			valid = false;
+		}
+		else
+		{
+			/*
+			 * The trigger field is valid.
+			 */
+
+			valid = true;
+			if ( trigger == InputTrigger.NOT_MAPPED )
+				tokens = new String[] {};
+			else
+				tokens = trigger.toString().split( " " );
+
+		}
+
+		if ( tokens.length == 0 )
+		{
+			trigger = InputTrigger.NOT_MAPPED;
+			invalidTriggerStr = null;
+		}
+		else
+		{
+			for ( final String key : tokens )
+			{
+				final KeyItem tagp = new KeyItem( key, valid );
+				keyItems.add( tagp );
+				add( tagp, getComponentCount() - 2 );
+			}
+		}
+
 		revalidate();
 		repaint();
 	}
@@ -183,12 +306,13 @@ public class KeyBindingsPanelEditor extends JPanel
 			if ( pos - w < 2 )
 				return;
 
-			final String prefix = content.substring( w + 1 ).toLowerCase();
-			final int n = Collections.binarySearch( INPUT_TRIGGER_SYNTAX_TAGS, prefix );
+			final String prefix = content.substring( w + 1 );
+			// We search on the lower case list.
+			final int n = Collections.binarySearch( INPUT_TRIGGER_SYNTAX_TAGS_SMALL_CAPS, prefix.toLowerCase() );
 			if ( n < 0 && -n <= INPUT_TRIGGER_SYNTAX_TAGS.size() )
 			{
 				final String match = INPUT_TRIGGER_SYNTAX_TAGS.get( -n - 1 );
-				if ( match.startsWith( prefix ) )
+				if ( match.toLowerCase().startsWith( prefix.toLowerCase() ) )
 				{
 					// A completion is found
 					final String completion = match.substring( pos - w );
@@ -206,19 +330,10 @@ public class KeyBindingsPanelEditor extends JPanel
 			@Override
 			public void actionPerformed( final ActionEvent ev )
 			{
-				final String key = textField.getText().trim().toUpperCase();
-				if ( selectedKeys.contains( key ) )
-				{
-					// Do not allow more than 1 tag instance.
-					textField.setText( "" );
-					return;
-				}
-
-				addKey( key );
+				final String key = textField.getText().trim();
+				checkAndAppendKey( key );
 				textField.setText( "" );
 				notifyListeners();
-				revalidate();
-				repaint();
 			}
 		}
 
@@ -247,80 +362,62 @@ public class KeyBindingsPanelEditor extends JPanel
 
 	}
 
-	final class KeyItem extends JPanel
+	private final class KeyItem extends JPanel
 	{
 
 		private static final long serialVersionUID = 1L;
 
 		public KeyItem( final String tag, final boolean valid )
 		{
-			final Font parentFont = KeyBindingsPanelEditor.this.getFont();
+			final Font parentFont = InputTriggerPanelEditor.this.getFont();
 			final Font font = parentFont.deriveFont( parentFont.getSize2D() - 2f );
-			final String str = TRIGGER_SYMBOLS.containsKey( tag ) ? TRIGGER_SYMBOLS.get( tag ) : tag;
+			final String str = TRIGGER_SYMBOLS.containsKey( tag ) ? ( " " + TRIGGER_SYMBOLS.get( tag ) + " " ) : ( " " + tag + " " );
 			final JLabel txt = new JLabel( str );
 			txt.setFont( font );
 			txt.setOpaque( true );
 			if ( !valid )
 				txt.setBackground( Color.PINK );
-			txt.setBorder( new RoundBorder( getBackground().darker(), KeyBindingsPanelEditor.this, 3 ) );
-
-			final JLabel close = new JLabel( "\u00D7" );
-			close.setOpaque( true );
-			close.setBackground( getBackground().darker().darker() );
-			close.setFont( font );
-			close.addMouseListener( new java.awt.event.MouseAdapter()
-			{
-				@Override
-				public void mousePressed( final java.awt.event.MouseEvent evt )
-				{
-					removeTag( tag );
-				}
-			} );
+			txt.setBorder( new RoundBorder( getBackground().darker(), InputTriggerPanelEditor.this, 1 ) );
 
 			setLayout( new BoxLayout( this, BoxLayout.LINE_AXIS ) );
-			if ( editable )
-				add( close );
 			add( Box.createHorizontalStrut( 1 ) );
 			add( txt );
-			add( Box.createHorizontalStrut( 4 ) );
+			add( Box.createHorizontalStrut( 1 ) );
 			setOpaque( false );
 		}
 	}
 
 	private void notifyListeners()
 	{
-		for ( final KeyBindingsChangeListener listener : listeners )
-			listener.keyBindingsChanged();
+		for ( final InputTriggerChangeListener listener : listeners )
+			listener.inputTriggerChanged();
 	}
 
-	public void addTagSelectionChangeListener( final KeyBindingsChangeListener listener )
+	public void addInputTriggerChangeListener( final InputTriggerChangeListener listener )
 	{
 		listeners.add( listener );
 	}
 
-	public void removeTagSelectionChangeListener( final KeyBindingsChangeListener listener )
+	public void removeInputTriggerChangeListener( final InputTriggerChangeListener listener )
 	{
 		listeners.remove( listener );
 	}
 
 	private static final List< String > INPUT_TRIGGER_SYNTAX_TAGS = new ArrayList<>();
-
+	private static final List< String > INPUT_TRIGGER_SYNTAX_TAGS_SMALL_CAPS;
 	private static final Map< String, String > TRIGGER_SYMBOLS = new HashMap<>();
-
 	static
 	{
 		for ( int i = 0; i < 26; i++ )
-		{
 			INPUT_TRIGGER_SYNTAX_TAGS.add( String.valueOf( ( char ) ( 'A' + i ) ) );
-			INPUT_TRIGGER_SYNTAX_TAGS.add( String.valueOf( ( char ) ( 'a' + i ) ) );
-			// Show small letters as upper case.
-			TRIGGER_SYMBOLS.put( String.valueOf( ( char ) ( 'a' + i ) ), String.valueOf( ( char ) ( 'A' + i ) ) );
-		}
 		for ( int i = 0; i < 10; i++ )
-			INPUT_TRIGGER_SYNTAX_TAGS.add( String.valueOf( ( char ) ( '0' + i ) ) );
+			INPUT_TRIGGER_SYNTAX_TAGS.add( "" + i );
+		for ( int i = 1; i <= 24; i++ )
+			INPUT_TRIGGER_SYNTAX_TAGS.add( "F" + i );
 
 		INPUT_TRIGGER_SYNTAX_TAGS.addAll(
 				Arrays.asList( new String[] {
+						"all",
 						"ENTER",
 						"BACK_SPACE",
 						"TAB",
@@ -374,12 +471,16 @@ public class KeyBindingsPanelEditor extends JPanel
 						"meta",
 						"win",
 						"double-click",
-						"button1 ",
-						"button2 ",
-						"button3 ",
-						"scroll"
+						"button1",
+						"button2",
+						"button3",
+						"scroll",
+						"|"
 				} ) );
-		INPUT_TRIGGER_SYNTAX_TAGS.sort( null );
+		INPUT_TRIGGER_SYNTAX_TAGS.sort( String.CASE_INSENSITIVE_ORDER );
+		INPUT_TRIGGER_SYNTAX_TAGS_SMALL_CAPS = new ArrayList<>(INPUT_TRIGGER_SYNTAX_TAGS.size());
+		for ( final String tag : INPUT_TRIGGER_SYNTAX_TAGS )
+			INPUT_TRIGGER_SYNTAX_TAGS_SMALL_CAPS.add( tag.toLowerCase() );
 
 		TRIGGER_SYMBOLS.put( "ENTER", "\u23CE" );
 		TRIGGER_SYMBOLS.put( "BACK_SPACE", "\u232B" );
@@ -408,14 +509,23 @@ public class KeyBindingsPanelEditor extends JPanel
 		TRIGGER_SYMBOLS.put( "NUMPAD9", "\u2468" );
 		TRIGGER_SYMBOLS.put( "MULTIPLY", "\u00d7" );
 		TRIGGER_SYMBOLS.put( "DIVIDE", "\u00f7" );
-		TRIGGER_SYMBOLS.put( "ADD", "\u00d7" );
+		TRIGGER_SYMBOLS.put( "ADD", "+" );
+		TRIGGER_SYMBOLS.put( "SUBTRACT", "-" );
+		TRIGGER_SYMBOLS.put( "COMMA", ",");
+		TRIGGER_SYMBOLS.put( "PERIOD", ".");
+		TRIGGER_SYMBOLS.put( "SLASH", "/" );
+		TRIGGER_SYMBOLS.put( "SEMICOLON", ";" );
+		TRIGGER_SYMBOLS.put( "EQUALS", "=");
+		TRIGGER_SYMBOLS.put( "OPEN_BRACKET", "[");
+		TRIGGER_SYMBOLS.put( "BACK_SLASH", "\\");
+		TRIGGER_SYMBOLS.put( "CLOSE_BRACKET", "]");
 		TRIGGER_SYMBOLS.put( "ctrl", "\u2303" );
 		TRIGGER_SYMBOLS.put( "alt", "\u2387" );
 		TRIGGER_SYMBOLS.put( "shift", "\u21e7" );
 		TRIGGER_SYMBOLS.put( "meta", "\u25c6" );
 		TRIGGER_SYMBOLS.put( "win", "\u2756" );
 		// Vertical bar is special
-		TRIGGER_SYMBOLS.put( "|", "  |  " );
+		TRIGGER_SYMBOLS.put( "|", "    |    " );
 	}
 
 }
