7cbd84778223aa5773c856e78c33b557cf5c4a99
Curtis Rueden <ctrueden@wisc.edu>
2023-08-16 15:32:50 -0500

    Happy New Year 2023

--- src/main/java/org/scijava/ui/behaviour/util/RunnableAction.java
+++ src/main/java/org/scijava/ui/behaviour/util/RunnableAction.java
@@ -2,7 +2,7 @@
  * #%L
  * Configurable key and mouse event handling
  * %%
- * Copyright (C) 2015 - 2022 Max Planck Institute of Molecular Cell Biology
+ * Copyright (C) 2015 - 2023 Max Planck Institute of Molecular Cell Biology
  * and Genetics.
  * %%
  * Redistribution and use in source and binary forms, with or without
